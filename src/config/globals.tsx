94b2e81962c3a424fbcef22cd3b1bd43eb7d4d06
chan04luis <xorashk.l.angel@gmail.com>
2025-02-20 23:18:43 -0600

    deploy

--- src/config/globals.tsx
+++ src/config/globals.tsx
@@ -1,3 +1,3 @@
 // config.js o constants.js
-export const apiUrl = "https://gownetwork.dyndns.org:445/api";
+export const apiUrl = "https://gownetwork.dyndns.org:446/api";
 export const errorServer = 'Hubo un error desconocido.';
